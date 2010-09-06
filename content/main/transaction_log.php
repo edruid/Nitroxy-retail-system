35506c3b65653e9062e1320cd164de41a1e15ba8
Eric Druid <edruid@kth.se>
2010-09-06 12:56:58 +0200

    closes #14 Transaction log view

---
+++ content/main/transaction_log.php
@@ -0,0 +1,20 @@
+<h1>Transaktionslogg</h1>
+<a href="transaction_log?page=<?=request_get('page')-1?>">&lt; föregående</a> <a href="transaction_log?page=<?=request_get('page')+1?>">nästa &gt;</a>
+<table class="body_border">
+	<? foreach(Transaction::selection(array('@order' => 'timestamp', '@limit' => array(request_get('page')*10, 10))) as $transaction): ?>
+		<tbody>
+			<tr>
+				<th colspan="2"><?=$transaction->timestamp?></th>
+				<th><?=$transaction->amount?> kr</th>
+			</tr>
+			<? foreach($transaction->TransactionContent() as $content): ?>
+				<tr>
+					<td><a href="/product/<?=$content->product_id?>"><?=$content->Product?></a></td>
+					<td><?=$content->count?> st</td>
+					<td><?=$content->amount?> kr</td>
+				</tr>
+			<? endforeach ?>
+		</tbody>
+	<? endforeach ?>
+</table>
+<a href="transaction_log?page=<?=request_get('page')-1?>">&lt; föregående</a> <a href="transaction_log?page=<?=request_get('page')+1?>">nästa &gt;</a>
