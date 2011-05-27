cd6de42dfe957f80b764607714a57db517850727
Eric Druid <edruid@kth.se>
2011-05-27 17:07:48 +0200

    Adding view of account transactions.

---
+++ content/main/account_transaction.php
@@ -0,0 +1,34 @@
+<?php
+if(empty($_SESSION['login'])) {
+	kick('login?kickback='.kickback_url());
+}
+$transaction = AccountTransaction::from_id(array_shift($request));
+$contents = $transaction->AccountTransactionContent;
+?>
+<h1>Transaktion - <p><?=$transaction->description?></h1>
+<table>
+	<tr>
+		<th>Användare</th>
+		<td><?=$transaction->user?></td>
+	</tr>
+	<tr>
+		<th>Tid</th>
+		<td><?=$transaction->timestamp?></td>
+	</tr>
+</table>
+<table>
+	<thead>
+		<tr>
+			<th>Konto</th>
+			<th>Summa</th>
+		</tr>
+	</thead>
+	<tbody>
+		<? foreach($contents as $content): ?>
+			<tr>
+				<td><?=$content->Account?></td>
+				<td><?=$content->amount?> kr</td>
+			</tr>
+		<? endforeach ?>
+	</tbody>
+</table>
