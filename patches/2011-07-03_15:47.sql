f2db1056139e4ed060a5edf693b3e6ed81d02e7e
Eric Druid <edruid@kth.se>
2011-07-10 11:36:14 +0200

    Some small steps towards in system users
    Please note that this is not runnable code.

---
+++ patches/2011-07-03_15:47.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `users` (
+	`user_id` int(10) unsigned not null,
+	`first_name` varchar(100) not null,
+	`surname` varchar(100) not null,
+	`username` varchar(100) not null,
+	PRIMARY KEY (`user_id`)
+) ENNGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+INSERT INTO users (`user_id`, `first_name`, `surname`, `username`) VALUES
+	(100, 'Eric', 'Druid', 'druid')
+	(, 'Robert', 'Lövlie', 'topace')
+	(, 'Uffe', 'Lilja', 'wolhay')
+	(, 'Kjell', 'Liden', 'kjellej')
+	(, 'Adam', 'Höse', 'adisblados')
+	(, 'Patrik', 'Roos', 'roos')
+	(, 'Peter', 'Eriksson', 'coopdot')
+
+ALTER TABLE account_transaction
