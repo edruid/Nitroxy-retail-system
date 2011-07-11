9deade43dd4a49244684f4cdf18bbed0afe2d8d9
Eric Druid <eric.druid@gmail.com>
2011-07-11 07:39:20 +0200

    Stop saving to product_log on each sale and grant access to the two remaining tables

--- grant.sql
+++ grant.sql
@@ -4,6 +4,8 @@ GRANT select, insert ON nitroxy_retail.account_transaction_contents TO 'nitroxy_
 GRANT select, insert, update, delete ON nitroxy_retail.categories TO 'nitroxy_retail'@'localhost';
 GRANT select, insert ON nitroxy_retail.deliveries TO 'nitroxy_retail'@'localhost';
 GRANT select, insert ON nitroxy_retail.delivery_contents TO 'nitroxy_retail'@'localhost';
+GRANT select, insert ON nitroxy_retail.product_log TO 'nitroxy_retail'@'localhost';
+GRANT select, insert ON nitroxy_retail.product_package TO 'nitroxy_retail'@'localhost';
 GRANT select, insert, update ON nitroxy_retail.products TO 'nitroxy_retail'@'localhost';
 GRANT select, insert ON nitroxy_retail.transaction_contents TO 'nitroxy_retail'@'localhost';
 GRANT select, insert ON nitroxy_retail.transactions TO 'nitroxy_retail'@'localhost';
