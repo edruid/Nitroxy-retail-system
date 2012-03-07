baa8786666fc59fb540f4e4e5490ebf2cd6c5b41
Eric Druid <eric.druid@gmail.com>
2012-03-07 08:46:32 +0100

    generate password script now takes password as argument

--- bin/generate_password.php
+++ bin/generate_password.php
@@ -2,11 +2,18 @@
 $chars = "qwertyuiopasdfghjkzxcvbnmQWERTYUPASDFGHJKLZXCVBNM123456789!#¤%&/()=?+";
 $length = 10;
 if(isset($_SERVER['argv']) && isset($_SERVER['argv'][1])) {
-	$length = $_SERVER['argv'][1];
+	$data = $_SERVER['argv'][1];
+	if(is_numeric($data) && $data > 0 && $data <= 999) {
+		$length = $data;
+	} else {
+		$password = $data;
+	}
 }
-$password = "";
-for($i = 0; $i<$length; $i++) {
-	$password .= $chars[rand() % strlen($chars)];
+if(!$password) {
+	$password = "";
+	for($i = 0; $i<$length; $i++) {
+		$password .= $chars[rand() % strlen($chars)];
+	}
 }
 $crypt = crypt($password, '$5$rounds=5000$'.rand());
 
